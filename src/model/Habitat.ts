420c1107a5d5637f367fee4442a43fdd01ae741e
Letícia Faria <le2006faria@gmail.com>
2024-02-14 09:00:22 -0300

    alterações na classe habitat, array

--- src/model/Habitat.ts
+++ src/model/Habitat.ts
@@ -2,9 +2,9 @@ import { Animal } from "./Animal";
 
 export class Habitat{
     private nome: string;
-    private lista_de_animais: Animal;
+    private lista_de_animais: Array<Animal>;
     
-    constructor(_nome: string, lista_de_animais: Animal){
+    constructor(_nome: string, lista_de_animais: Array<Animal>){
     this.nome= _nome;
     this.lista_de_animais= lista_de_animais;
     }
@@ -16,11 +16,11 @@ export class Habitat{
         this.nome = _nome;
     }
 
-    public getLista_de_animais() {
+    public getLista_de_animais(): Array<Animal> {
         return this.lista_de_animais;
     }
     public setLista_de_animais(_lista_de_animais: Animal) {
-        this.lista_de_animais = _lista_de_animais;
+        this.lista_de_animais.push(_lista_de_animais);
     }
 
 }
